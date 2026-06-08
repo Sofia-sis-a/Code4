b97eb5c8e541aeb5dc5fcf3058f7f958d754a07c
Sofia-sis-a <sofiakozlova23@gmail.com>
2026-06-08 13:24:54 +0300

    Update CallTracker.cpp

--- CallTracker.cpp
+++ CallTracker.cpp
@@ -32,7 +32,7 @@ CallTracker::~CallTracker() {
     delete[] phoneNumber;
 }
 
-double CallTracker::MaxwellDuration() const { return maxCallDuration; }
+double CallTracker::getMaxDuration() const { return maxCallDuration; }
 double CallTracker::getMinDuration() const { return minCallDuration; }
 double CallTracker::getTotalDuration() const { return totalDuration; }
 int CallTracker::getTotalCalls() const { return totalCalls; }
@@ -116,37 +116,4 @@ std::istream& operator >> (std::istream& in, CallTracker& ourObject) {
 double& operator += (double& sum, const CallTracker& ourObject) {
     sum += ourObject.getTotalDuration();
     return sum;
-}
-
-ImportantCallTracker::ImportantCallTracker(const char* phone, const char* name) 
-    : CallTracker(phone, name), totalRating(0.0), currentRating(0.0) {}
-
-double ImportantCallTracker::getTotalRating() const { return totalRating; }
-double ImportantCallTracker::getCurrentRating() const { return currentRating; }
-
-double ImportantCallTracker::getAverageRating() const {
-    if (totalCalls == 0) {
-        return 0.0;
-    }
-    return totalRating / totalCalls;
-}
-
-void ImportantCallTracker::addCall(double duration, double rating) {
-    if (rating < 0 || rating > 5) {
-        throw std::out_of_range("Error: Rating must be between 0 and 5!");
-    }
-    CallTracker::addCall(duration);
-    currentRating = rating;
-    totalRating += rating;
-}
-
-void ImportantCallTracker::printInfo() const {
-    std::cout << "========================================" << std::endl;
-    std::cout << "Абонент (ВАЖНЫЙ): " << subscriberName << std::endl;
-    std::cout << "Телефон: " << phoneNumber << std::endl;
-    std::cout << "Всего звонков: " << totalCalls << std::endl;
-    std::cout << "Общая длительность: " << totalDuration << " мин." << std::endl;
-    std::cout << "Последний рейтинг звонка: " << currentRating << std::endl;
-    std::cout << "Средний рейтинг абонента: " << getAverageRating() << std::endl;
-    std::cout << "========================================" << std::endl;
 }
